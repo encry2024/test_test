91768a397217cf6fb648af92692c009d9a3e22cc
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2018-02-28 11:52:17 +0800

    Removed some unncessary welcome panel

--- app/Models/Client/Client.php
+++ app/Models/Client/Client.php
@@ -3,8 +3,25 @@
 namespace App\Models\Client;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Client\Traits\Attirbute\ClientAttribute;
+use App\Models\Client\Traits\Relationship\ClientRelationship;
 
 class Client extends Model
 {
-    //
+    use SoftDeletes,
+        ClientAttribute,
+        ClientRelationship;
+
+    protected $fillable = [
+        'name',
+        'contact_person_first_name',
+        'contact_person_last_name',
+        'contact_person_email',
+        'contact_person_contact_number',
+        'address'
+    ];
+
+    protected $dates = ['deleted_at'];
+
 }
